faf168a6d85fa8d53bbe73aff86ce639bc6a613b
Albert Cadena <albert.cad.rub@gmail.com>
2017-05-21 20:54:18 +0200

    Modificacion, ahora se carga toda la info de los Estilos en el MisEstilosActivity

--- estilos/listarEstilos.php
+++ estilos/listarEstilos.php
@@ -5,7 +5,7 @@ require '../connection.php';
 if ($_SERVER['REQUEST_METHOD'] == 'GET' || $_SERVER['REQUEST_METHOD'] == 'POST') {
     if(isset($_REQUEST["id_usuario"]) && isset($_REQUEST["tipoConsulta"])){
         if($_REQUEST["tipoConsulta"] == "1"){
-            Connexio::queryListaEstilosSoloIdNombre($_REQUEST["id_usuario"]);
+            Connexio::queryListaEstilosCompletos($_REQUEST["id_usuario"]);
         }else if($_REQUEST["tipoConsulta"] == "2"){
             Connexio::queryListaInfoTablaEstilos();
         }else{
